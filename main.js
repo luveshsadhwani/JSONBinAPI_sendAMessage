04f64a1434105b124495f6097b70333f8cd4c25c
Luvesh L Sadhwani <luveshsadhwani@gmail.com>
2023-03-16 02:07:15 +0800

    update keys and rewrite logic

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-const urlBin = "https://api.jsonbin.io/b";
-const secretKey = "";
-const collectionID = "";
+const baseUrl = "https://api.jsonbin.io/v3";
+
+const secretKey =
+  "$2b$10$uM/miVxgt6UyLkG1pND34OOoWf6RgINgrp5th7HJHADqC3dA9uNHi";
+const collectionID = "6411fb6debd26539d08f3024";
+const accessKey =
+  "$2b$10$f4XCTcxJ7k4G9W7O8MJCPubiKrn8e19p8hzgnR9HHawfSOTrmBJAS";
 
 // Creates a new message and sends this to JSONBin
 const createBin = () => {
@@ -16,15 +20,18 @@ const createBin = () => {
     }
   };
 
-  xhr.open("POST", urlBin);
-  xhr.setRequestHeader("Content-type", "application/json");
-  xhr.setRequestHeader("secret-key", secretKey);
-  xhr.setRequestHeader("collection-id", collectionID);
+  const url = `${baseUrl}/b`;
+
+  xhr.open("POST", url);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.setRequestHeader("X-Master-Key", secretKey);
+  xhr.setRequestHeader("X-Access-Key", accessKey);
+  xhr.setRequestHeader("X-Collection-Id", collectionID);
   xhr.send(data);
 };
 
 //DOM elements
-const btn = document.getElementById("mySubmit");
+const btn = document.getElementById("submit-btn");
 const usernameLabel = document.getElementById("username");
 const messageLabel = document.getElementById("message");
 
